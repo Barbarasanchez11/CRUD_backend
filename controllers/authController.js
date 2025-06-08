09b6cabbe654ea95ad05fe8f6ab23fc5110bdc33
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-06-08 17:49:30 +0200

    feat:create baseHtml auxiliar funciton

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+const Task = require('../models/Task')
+const {baseHtml} = require('../public/utils/index')
+
+const authController = {
+
+    async showNewTask(req,res){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = authController
---
+++ public/utils/index.js
@@ -0,0 +1,20 @@
+function baseHtml(){
+    return `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tienda de Productos</title>
+        <link rel="stylesheet" href="/styles/style.css">
+    </head>
+    <body>
+        <header>
+            
+        </header>
+    </body>
+    </html>
+    `;
+}
+
+module.exports = {baseHtml}
